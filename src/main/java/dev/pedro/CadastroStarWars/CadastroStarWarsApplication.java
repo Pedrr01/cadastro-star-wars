9a6660f331c2645dfca96a4f398057f5dfecc5e9
Pedro Vitor <pedrovitor.silva14vs@gmail.com>
2025-09-11 13:34:53 -0300

    first commit

---
+++ src/main/java/dev/pedro/CadastroStarWars/CadastroStarWarsApplication.java
@@ -0,0 +1,13 @@
+package dev.pedro.CadastroStarWars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroStarWarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroStarWarsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/pedro/CadastroStarWars/CadastroStarWarsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.pedro.CadastroStarWars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroStarWarsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
