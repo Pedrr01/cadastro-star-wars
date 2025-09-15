a054a1d41482492ec1961861654e4d96da6a5797
Pedro Vitor <pedrovitor.silva14vs@gmail.com>
2025-09-15 20:49:07 -0300

    foi criada a entidade de cadastro PersonagemModel

---
+++ src/main/java/dev/pedro/CadastroStarWars/PersonagemModel.java
@@ -0,0 +1,48 @@
+package dev.pedro.CadastroStarWars;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_cadastro")
+public class PersonagemModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private int idade;
+
+    public PersonagemModel(){
+    }
+
+    public PersonagemModel(String nome, String email, int idade){
+        this.nome = nome;
+        this.email = email;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+}
